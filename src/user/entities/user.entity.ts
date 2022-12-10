0a7db8ddfb505d1daf8aab58b253b88dbeb6bdb4
Jack Tri Le <Jack>
2022-12-10 23:48:50 +0800

    first commit

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column()
+  userName: string;
+
+  @Column()
+  password: string;
+}
